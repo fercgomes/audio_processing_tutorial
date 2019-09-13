bcfbeec55f511c01fe44276561547fa8696a1c7b
Rodrigo Schramm <schramm@Rodrigos-MacBook-Pro.local>
2019-09-12 23:56:34 -0300

    ...

--- webaudio/worklet/gain-processor.js
+++ webaudio/worklet/gain-processor.js
@@ -20,7 +20,7 @@ class GainProcessor extends AudioWorkletProcessor {
       const outputChannel = output[channel];
       if (gain.length === 1) {
         for (let i = 0; i < inputChannel.length; ++i)
-          outputChannel[i] = inputChannel[i] * gain[0];
+          outputChannel[i] = inputChannel[i] * gain[0] + randi;
       } else {
         for (let i = 0; i < inputChannel.length; ++i)
           outputChannel[i] = inputChannel[i] * gain[i];
@@ -33,3 +33,36 @@ class GainProcessor extends AudioWorkletProcessor {
 
 registerProcessor('gain-processor', GainProcessor);
 
+
+/*
+//  https://developer.mozilla.org/en-US/docs/Web/API/AudioWorkletProcessor/parameterDescriptors 
+//  
+ // A separate script file, like "my-worklet-processor.js" 
+class MyWorkletProcessor extends AudioWorkletProcessor {
+
+  // Static getter to define AudioParam objects in this custom processor.
+  static get parameterDescriptors() {
+    return [{
+      name: 'myParam',
+      defaultValue: 0.707
+    }];
+  }
+
+  constructor() { super(); }
+
+  process(inputs, outputs, parameters) {
+    // |myParamValues| is a Float32Array of either 1 or 128 audio samples
+    // calculated by WebAudio engine from regular AudioParam operations.
+    // (automation methods, setter) Without any AudioParam change, this array
+    // would be a single value of 0.707.
+    const myParamValues = parameters.myParam;
+
+    if (myParamValues.length === 1) {
+      // |myParam| has been a constant value for the current render quantum,
+      // which can be accessed by |myParamValues[0]|.
+    } else {
+      // |myParam| has been changed and |myParamValues| has 128 values.
+    }
+  }
+}
+*/
